7fbcdbd889a3501cbf4738a12f60e04ddcca1cd2
HongkyunKwon <kaylen@unist.ac.kr>
2024-05-24 17:09:26 +0900

    good

--- main.py
+++ main.py
@@ -6,54 +6,101 @@ import torchvision.transforms as transforms
 import torch.nn.functional as F
 from tqdm import tqdm
 import argparse
+from tqdm import tqdm
+import PIL
+import matplotlib.pyplot as plt
+from torch.utils.data.dataloader import DataLoader
 
-def train(model, train_loader, optimizer, criterion):
-    EPS = 1e-6
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def mkds(args, dataset):
+    fulldataset = []
+    num_data = 10000
+    classes = dataset.classes
+    class_per_data = num_data/len(classes)
+    num_original = int((1 - args.rate) * class_per_data)
+    num_generated = int(args.rate * class_per_data)
+    print('num of original data : ',num_original)
+    print('num of generated data : ',num_generated)
+    for i in range(len(classes)):
+        count = 0
+        for j in range(len(dataset)):
+            if dataset.targets[j] == i:
+                fulldataset.append(dataset[j])
+                count = count + 1
+            if count >= num_original:
+                break
+    if args.dataset == 'mnist':
+        for i in range(len(classes)):
+            for j in range(int(num_generated)):
+                transform = transforms.Compose([transforms.Resize((28,28)), transforms.ToTensor()])
+                img = transform(PIL.Image.open(f'./mnist/generated_dataset/{i}/MNIST-{i}-{j}.png'))
+                fulldataset.append((img,i))
+    elif args.dataset == 'cifar10':
+        for i in range(len(classes)):
+            for j in range(int(num_generated)):
+                transform = transforms.Compose([transforms.ToTensor()])
+                img = transform(PIL.Image.open(f'./cifar10/generated_dataset/{classes[i]}/{j}.png'))
+                fulldataset.append((img,i))
+    return fulldataset
     
-    for batch_idx, (imgs, targets) in enumerate(train_loader):
-        optimizer.zero_grad()
-        #imgs, targets = next(train_loader)
-        imgs, targets = imgs.to(device), targets.to(device)
-        output = model(imgs)
-        train_loss = criterion(output, targets)
-        train_loss.backward()
-
-        
-    return train_loss.item()
-
 def main(args):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     epochs = 100
-    transform=transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.1307,), (0.3081,))])
-    if args.dataset == "mnist":
-        traindataset = datasets.MNIST('../data', train=True, download=True,transform=transform)
-        testdataset = datasets.MNIST('../data', train=False, transform=transform)
-        from MNIST import LeNet5
-        model = LeNet5()
+    if args.dataset == "mnist": #generated 데이터 32x32 resize
+        from mnist import LeNet5
+        transform = transforms.Compose([transforms.ToTensor()])
+        traindataset = datasets.MNIST('../data', train=True, download=True, transform= transform)
+        testdataset = datasets.MNIST('../data', train=False, download=True, transform= transform) 
     elif args.dataset =="cifar10":
-        traindataset = datasets.CIFAR10('../data', train=True, download=True,transform=transform)
-        testdataset = datasets.CIFAR10('../data', train=False, transform=transform) 
-        from Cifar10 import LeNet5
-        model = LeNet5()
-
-
+        from cifar10 import LeNet5
+        transform = transforms.Compose([transforms.ToTensor()])
+        traindataset = datasets.CIFAR10('../data', train=True, download=True, transform= transform)
+        testdataset = datasets.CIFAR10('../data', train=False, download=True, transform= transform) 
+    
+    traindataset = mkds(args, traindataset)
+    model = LeNet5.LeNet5().to(device)
     criterion = nn.CrossEntropyLoss() 
     optimizer = torch.optim.Adam(model.parameters(), weight_decay=1e-4)
-    traindataset_data = traindataset.data[:1000]
-    traindataset_target = traindataset.targets[:1000]
-
-
+    train_loader = DataLoader(traindataset, batch_size=16, shuffle= True)
+    test_loader = DataLoader(testdataset, batch_size = 16)
     model.train()
-    for epoch in epochs:
-        
-
-        optimizer.step()
-        pass
+    total_loss = []
+    pbar = tqdm(range(epochs))
+    for epoch in pbar:
+        loss = 0
+        for batch_idx, (imgs, targets) in enumerate(train_loader):
+            imgs = imgs.to(device)
+            targets = targets.to(device)
+            optimizer.zero_grad()
+            output = model(imgs)
+            train_loss = criterion(output, targets)
+            train_loss.backward()
+            optimizer.step()
+            loss = loss + train_loss
+        pbar.set_description(f'Train Epoch: {epoch}/{epochs} Loss : {loss:.6f}')
+        total_loss.append(loss.cpu().detach().numpy())
+    plt.plot(total_loss)
+    plt.show()
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).sum().item()
+        test_loss /= len(test_loader.dataset)
+        accuracy = 100. * correct / len(test_loader.dataset)
+    print('accuracy : ', accuracy)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", default="mnist", type=str, help="mnist | cifar10")
-    parser.add_argument("--rate", default= 0.7, type = float )
+    parser.add_argument("--rate", default= 0.1, type = float )
 
     args = parser.parse_args()
     main(args)
